8161d73d54175c4157d5106fc74675c6fdb7d08e
alot290 <qbs1864@outlook.com>
2020-04-24 02:35:58 -0400

    menu button addedd to navbar

---
+++ hamburger.js
@@ -0,0 +1,9 @@
+// $(document).ready(function(){
+//
+// });
+function burgerFunction() {
+  console.log("burger")
+  var element = document.getElementById("hamburger");
+   element.classList.toggle("nav-icon1");
+   element.classList.toggle("nav-icon2");
+}
