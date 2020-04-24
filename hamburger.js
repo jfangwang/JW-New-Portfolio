0a434df8ff41de15c6dd03970a7892212631d82f
alot290 <qbs1864@outlook.com>
2020-04-24 08:21:03 -0400

    added menu

--- hamburger.js
+++ hamburger.js
@@ -1,9 +1,54 @@
-// $(document).ready(function(){
-//
-// });
+function pageLoaded() {
+
+
+}
 function burgerFunction() {
-  console.log("burger")
+  console.log("burger");
   var element = document.getElementById("hamburger");
    element.classList.toggle("nav-icon1");
    element.classList.toggle("nav-icon2");
+   // element.onclick.toggle("enableScroll()");
+  var element = document.getElementById("navMenu")
+   element.classList.toggle("nav-menu-open");
+   element.classList.toggle("nav-menu-close");
+}
+
+var keys = {37: 1, 38: 1, 39: 1, 40: 1};
+
+function preventDefault(e) {
+  e.preventDefault();
+}
+
+function preventDefaultForScrollKeys(e) {
+  if (keys[e.keyCode]) {
+    preventDefault(e);
+    return false;
+  }
+}
+
+// modern Chrome requires { passive: false } when adding event
+var supportsPassive = false;
+try {
+  window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
+    get: function () { supportsPassive = true; }
+  }));
+} catch(e) {}
+
+var wheelOpt = supportsPassive ? { passive: false } : false;
+var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
+
+// call this to Disable
+function disableScroll() {
+  window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
+  window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
+  window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
+  window.addEventListener('keydown', preventDefaultForScrollKeys, false);
+}
+
+// call this to Enable
+function enableScroll() {
+  window.removeEventListener('DOMMouseScroll', preventDefault, false);
+  window.removeEventListener(wheelEvent, preventDefault, wheelOpt);
+  window.removeEventListener('touchmove', preventDefault, wheelOpt);
+  window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
 }
